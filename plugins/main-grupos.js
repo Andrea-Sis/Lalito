bab6d16289bca6875b3100c0a35a5c40975c19bc
Andrea-Sis <andreaa.valen21@gmail.com>
2024-10-31 23:49:12 -0400

    Update main-grupos.js

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -6,7 +6,7 @@ const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 let txt = `*Hola!, te invito a unirte a los grupos oficiales del Bot para convivir con la comunidad :D*
 
-> ⭐ _Grupo Oficial_ 𝑺𝑰𝑺𝑲𝑬𝑫 𝑩𝑶𝑻
+> ❄️ Unete al Grupo Oficial de 𝑩𝑶𝑻 𝑾𝑨𝑳𝑳•𝑬
 
 *🎌* ${group}
 
